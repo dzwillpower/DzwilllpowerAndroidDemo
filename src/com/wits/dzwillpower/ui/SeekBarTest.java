edb52b59e04a665368c22ec0a6c84b26d171c622
dongzhi <dongzhi@oppo.com>
2015-03-19 10:38:10 +0800

    增加 seekbar 测试代码

--- src/com/wits/dzwillpower/ui/SeekBarTest.java
+++ src/com/wits/dzwillpower/ui/SeekBarTest.java
@@ -3,12 +3,10 @@ package com.wits.dzwillpower.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ViewGroup;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import com.wits.dzwillpower.android.R;
-import com.wits.dzwillpower.android.widget.StartPointSeekBar;
-import com.wits.dzwillpower.android.widget.StartPointSeekBar.OnSeekBarChangeListener;
 
 
 public class SeekBarTest extends Activity {
@@ -18,45 +16,47 @@ public class SeekBarTest extends Activity {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_progress);
-//        mSeekBar = (SeekBar)findViewById(R.id.seek_bar);
-//        mSeekBar.setMax(1000);
-//        mSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//                // TODO Auto-generated method stub
-//
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//                // TODO Auto-generated method stub
-//
-//            }
-//
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//
-//              Log.d("SeekBarTest", "progress: "+progress);
-//
-//            }
-//        });
+        mSeekBar = (SeekBar)findViewById(R.id.seek_bar);
+        mSeekBar.setMax(1000);
+        mSeekBar.setThumbOffset(0);
+        mSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 
-
-
-        StartPointSeekBar seekBar = new StartPointSeekBar(0, +100, this);
-        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener<Integer>()
-        {
             @Override
-            public void onOnSeekBarValueChange(StartPointSeekBar<?> bar, Integer value)
-            {
-                Log.d("SeekBarTest", "seekbar value:" + value);
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
             }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+              Log.d("SeekBarTest", "progress: "+progress);
+
+            }
+
         });
 
-        // add RangeSeekBar to pre-defined layout
-        ViewGroup layout = (ViewGroup) findViewById(R.id.seekbarwrapper);
-        layout.addView(seekBar);
+
+
+//        StartPointSeekBar seekBar = new StartPointSeekBar(0, +100, this);
+//        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener<Integer>()
+//        {
+//            @Override
+//            public void onOnSeekBarValueChange(StartPointSeekBar<?> bar, Integer value)
+//            {
+//                Log.d("SeekBarTest", "seekbar value:" + value);
+//            }
+//        });
+//
+//        // add RangeSeekBar to pre-defined layout
+//        ViewGroup layout = (ViewGroup) findViewById(R.id.seekbarwrapper);
+//        layout.addView(seekBar);
     }
 
 }
