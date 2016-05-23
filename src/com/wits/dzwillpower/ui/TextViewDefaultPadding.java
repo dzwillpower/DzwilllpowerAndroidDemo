fdf2fa5b6926fc134a6a1ceeda8378e83f93e268
dongzhi <dongzhi@oppo.com>
2016-05-23 15:09:15 +0800

    修改测试textview的默认padding

---
+++ src/com/wits/dzwillpower/ui/TextViewDefaultPadding.java
@@ -0,0 +1,16 @@
+package com.wits.dzwillpower.ui;
+
+import com.wits.dzwillpower.android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TextViewDefaultPadding extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.textview_default_padding);
+    }
+
+}
